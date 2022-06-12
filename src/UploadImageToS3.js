d24a44bc46a056d36c5481c3487c7190493c26f7
Jeff Ginn <jeffginn@jeffs-mbp.lan>
2022-06-12 17:27:25 -0400

    adding simple canvas

--- src/UploadImageToS3.js
+++ src/UploadImageToS3.js
@@ -1,71 +1,115 @@
 import React, {useState} from 'react';
 
 // TODO - pass in as an environment variable
-const serverUrl = 'http://localhost:3000'
+const serverUrl = 'https://0fyuj0x77e.execute-api.us-east-1.amazonaws.com'
 
-let drawing;
+let segmentPromise;
 
-const UploadImageToS3 = () => {
-    const [selectedFile, setSelectedFile] = useState(null);
+async function realizeSegment(priorDrawingId, priorPlacement) {
+    let drawing = await realizeDrawing(priorDrawingId, priorPlacement);
 
-    const handleFileInput = (e) => setSelectedFile(e.target.files[0]);
+    let placement;
 
-    const uploadFile = async (file, segment) => {
-        if (!drawing) {
-            drawing = await createDrawing();
-        }
-        const body = {
-            contentType: "image/jpeg",
-            ext: "jpg",
-            createdBy: "Sam Segment"
-        }
-
-        const requestObject = {
-            method: 'POST',
-            body: JSON.stringify(body),
-            headers: {
-                "Content-Type": "application/json",
-            },
-            mode: "cors"
-        }
-
-        const url = serverUrl + '/drawings/' + drawing.id + '/segments/' + segment;
-
-        fetch(url, requestObject)
-            .then(res => res.json())
-            .then(data => {
-                fetch(data.preSignedUrl, {
-                    method: 'PUT',
-                    body: file
-                }).then((res) => {
-                    console.log('Upload succeeded!')
-                }).catch(err => {
-                    console.log(err)
-                })
-            })
+    if (!drawing.hasOwnProperty('top') && priorPlacement !== 'top') {
+        placement = 'top';
+    } else if (!drawing.hasOwnProperty('middle') && priorPlacement !== 'middle') {
+        placement = 'middle';
+    } else if (!drawing.hasOwnProperty('bottom') && priorPlacement !== 'bottom') {
+        placement = 'bottom';
+    } else {
+        throw Error('drawing has all segments');
     }
 
-
-    return <>
-        <div>Image</div>
-        <input type="file" onChange={handleFileInput}/>
-
-        <div>Top</div>
-        <button onClick={() => uploadFile(selectedFile, 'top')}> Upload as top</button>
-
-        <div>Middle</div>
-        <button onClick={() => uploadFile(selectedFile, 'middle')}> Upload as middle</button>
-
-        <div>Bottom</div>
-        <button onClick={() => uploadFile(selectedFile, 'bottom')}> Upload as bottom</button>
-    </>
+    return {
+        placement: placement,
+        drawing: drawing
+    }
 }
 
-async function createDrawing() {
+async function realizeDrawing(priorDrawingId, priorPlacement) {
     const url = serverUrl + '/drawings';
 
+    const getDrawingsRequest = {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        mode: "cors"
+    }
+
+    const getDrawingsResponse = await fetch(url, getDrawingsRequest);
+    const drawings = await getDrawingsResponse.json();
+
+    // Find a drawing that's missing a segment other than the segment we just created
+    const drawing = drawings.find(drawing =>
+        (!drawing.hasOwnProperty('top') && (drawing.id !== priorDrawingId || priorPlacement !== 'top')) ||
+        (!drawing.hasOwnProperty('middle') && (drawing.id !== priorDrawingId || priorPlacement !== 'middle')) ||
+        (!drawing.hasOwnProperty('bottom') && (drawing.id !== priorDrawingId || priorPlacement !== 'bottom')));
+
+    if (drawing) {
+        return drawing;
+    }
+
+    // No drawing found, create a drawing
     const body = {
         createdBy: "Dawn Drawing"
+    }
+
+    const createDrawingRequest = {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json",
+        },
+        mode: "cors"
+    }
+
+    const response = await fetch(url, createDrawingRequest);
+    return await response.json();
+}
+
+const SaveImage = async () => {
+    let canvas = document.getElementById("canvas")
+    let data = canvas.toDataURL("image/jpeg")
+    const blobData = dataURItoBlob(data);
+    await uploadFile(blobData)
+}
+
+function dataURItoBlob(dataURI) {
+    const binary = atob(dataURI.split(',')[1]);
+    const array = [];
+    for (let i = 0; i < binary.length; i++) {
+        array.push(binary.charCodeAt(i));
+    }
+    return new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
+}
+
+const ClearCanvas = () => {
+    let canvas = document.getElementById("canvas")
+    let ctx = canvas.getContext("2d")
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+const reset = (priorDrawingId, priorPlacement) => {
+    segmentPromise = realizeSegment(priorDrawingId, priorPlacement);
+
+    segmentPromise.then((segment) => {
+        document.getElementById('currentSegment').innerHTML = 'You are creating the ' + segment.placement + ' of drawing id ' + segment.drawing.id;
+    });
+
+    ClearCanvas();
+}
+
+const uploadFile = async (file) => {
+    let segment = await segmentPromise;
+
+    if (!segment)
+        throw Error('Segment not selected')
+
+    const body = {
+        contentType: "image/jpeg",
+        ext: "jpg",
+        createdBy: "Sam Segment"
     }
 
     const requestObject = {
@@ -77,8 +121,31 @@ async function createDrawing() {
         mode: "cors"
     }
 
-    const response = await fetch(url, requestObject);
-    return await response.json();
+    const url = serverUrl + '/drawings/' + segment.drawing.id + '/segments/' + segment.placement;
+
+    fetch(url, requestObject)
+        .then(res => res.json())
+        .then(data => {
+            fetch(data.preSignedUrl, {
+                method: 'PUT',
+                body: file
+            }).then((res) => {
+                console.log('Upload succeeded!')
+                reset(segment.drawing.id,segment.placement);
+            }).catch(err => {
+                alert('Error uploading segment')
+                console.log(err)
+            })
+        })
+}
+
+reset(0,0);
+
+const UploadImageToS3 = () => {
+    return <>
+        <button className="clear" onClick={ClearCanvas}>clear</button>
+        <button className="save" onClick={SaveImage}>save</button>
+    </>
 }
 
 export default UploadImageToS3;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import UploadImageToS3 from "./UploadImageToS3";
+
+let canvas = document.getElementById("canvas")
+canvas.height = 500
+canvas.width = 500
+let ctx = canvas.getContext("2d")
+ctx.lineWidth = 5
+
+let prevX = null
+let prevY = null
+
+let draw = false
+
+let clrs = document.querySelectorAll(".clr")
+clrs = Array.from(clrs)
+clrs.forEach(clr => {
+    clr.addEventListener("click", () => {
+        ctx.strokeStyle = clr.dataset.clr
+    })
+})
+
+window.addEventListener("mousedown", (e) => draw = true)
+window.addEventListener("mouseup", (e) => draw = false)
+
+window.addEventListener("mousemove", function(e){
+    if(prevX == null || prevY == null || !draw){
+        prevX = e.offsetX
+        prevY = e.offsetY
+        return
+    }
+
+    let mouseX = e.offsetX
+    let mouseY = e.offsetY
+    ctx.beginPath()
+    ctx.moveTo(prevX, prevY)
+    ctx.lineTo(mouseX, mouseY)
+    ctx.stroke()
+
+    prevX = e.offsetX
+    prevY = e.offsetY
+})
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <UploadImageToS3 />
-  </React.StrictMode>
+    <React.StrictMode>
+        <UploadImageToS3 />
+    </React.StrictMode>
 );
